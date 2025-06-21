b8c2e5d80fee1223e0d94b4723d34e9d7b3aebb3
ekakshjanweja <jekaksh@gmail.com>
2025-06-21 22:05:07 +0530

    misc

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,12 +6,9 @@ import { auth } from "@/lib/auth";
 import { useRouter } from "next/navigation";
 import { useEffect } from "react";
 
-export default  async function Home() {
+export default function Home() {
   const router = useRouter();
   const { data: session, isPending } = auth.useSession();
-
-    const s = await auth.getSession();
-
 
   useEffect(() => {
     if (session?.user) {
@@ -38,13 +35,11 @@ export default  async function Home() {
     return <Loading />;
   }
 
-
   return (
     <div>
       <main className="flex items-center justify-center min-h-[calc(100vh-80px)]">
         <div className="text-center space-y-6">
           <div className="space-y-2">
-            <p>{JSON.stringify(s.data)}</p>
             <h1 className="text-4xl font-bold">Welcome to Turi</h1>
             <p className="text-muted-foreground text-lg">
               Sign in to get started
